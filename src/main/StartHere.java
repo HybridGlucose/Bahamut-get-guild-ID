dfedc0e6daba0e1d0aa7b59eb14eff08800997e7
HybridGlucose <a07051226@gmail.com>
2015-07-18 00:29:40 +0800

    v0.1

--- src/main/StartHere.java
+++ src/main/StartHere.java
@@ -1,35 +1,62 @@
 package main;
-
+/*
+ 	巴哈姆特私信抓帳號v0.1
+ 	本程式由HybridGlucose（a07051226@gmail.com，巴哈ID：aa07051226）撰寫，特別寫給「巴哈姆特《歷史の紳士公會<ゝω・》」專用
+	本程式用於抓取私信串帳號，目前功能尚未完整
+ 	
+ 	本程式具有以下功能：
+ 	1.可指定關鍵字
+ 	2.利用關鍵字抓該留言者帳號
+ 	3.自動產生私信用帳號格式
+ 	
+ 	本程式不具有以下功能，預計未來推出
+ 	1.每20個帳號分行
+	2.最後尾的「,」須手動刪除
+	3.圖形化界面
+ 	
+ 	 //版權聲明
+ 	 * 請勿改最此程式，本人(HybridGlucose)具有本程式著作權
+	 * 請勿進行任何改作
+	 * 非經本人同意請勿散發本程式
+	
+	2015/07/18
+*/
 import java.io.*;
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Scanner;
+import java.util.Set;
 
 public class StartHere {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws IOException 
 	{
 		// TODO 自動產生的方法 Stub
-		System.out.print("請輸入檔案路徑：");
 		Scanner scn = new Scanner(System.in);
+		System.out.print("請輸入檔案路徑（例如C://ID.txt）：");
 		String filePath = scn.next();
-		/*System.out.print("請輸入行數：");
-		String lineNum = scn.next();
-		int num = Integer.parseInt(lineNum);*/
+		System.out.print("請輸入篩選關鍵字：");
+		String keyWord = scn.next();
+		System.out.print("請輸出檔案路徑（例如C://out.txt，請注意，檔案會直接複寫）：");
+		String outPath = scn.next();
+		String outputID = "";
+		Set<String> idList = new HashSet<String>();
+		scn.close();
 		try
 		{
-			//String str = "all +=buildReply(75152391,'calaylee2023','アッカリ〜ン蒔六夜','calaylee2023,','8小時前',0,21705604,13,'');";
-			//String[] arrStr0 = str.split("\\'");
-			//System.out.println(arrStr0[5].toString());
 			//接收原始檔輸入
 			FileReader fr = new FileReader(filePath);
 			BufferedReader br = new BufferedReader(fr);
-			String line,tempString;
-			//String[] tempArray = new String[num];
-			int i = 0;
+			String line;
 			while((line = br.readLine())!= null)
 			{
-							String[] aryS = line.split("\\'*\\'"); 
-							System.out.println(aryS[5].trim()); 
+				String[] aryS = line.split("\\'*\\'");
+				if (aryS[5].equals(keyWord))
+				{
+					idList.add(aryS[1]);//[1]為id位置，[5]為內容位置
+					//System.out.print(aryS[5]);
+					//System.out.print(aryS[1]+",");
+				}
 			}
 		} catch (FileNotFoundException e)
 		{
@@ -41,7 +68,16 @@ public class StartHere {
 		}
 		finally
 		{
-			
+			Iterator<String> listViewer = idList.iterator();
+			while (listViewer.hasNext())
+			{
+				outputID += listViewer.next() + ",";
+			}
+			BufferedWriter outFile = new BufferedWriter(new FileWriter(outPath));
+			outFile.write(outputID);
+			outFile.close();
+			System.out.println("\n\n\n檔案輸出位置為"+outPath);
+			System.out.println("最後尾的「,」不會自動刪除，請手動刪除它");
 		}
 	}
 
